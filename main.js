2e88bf4f30a4fa0ff719237daa02f1083effa8b7
hellotheredanhere <refreshingperspective@protonmail.com>
2021-11-06 09:44:23 -0400

    testing showing results on the page

--- main.js
+++ main.js
@@ -7,33 +7,42 @@ const app = express()
 
 const url ='https://www.indeed.com/jobs?q=junior%20web%20developer&l=Remote&vjk=92343f459a8aa675'
 
-axios(url)
-    .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const articles = []
-                
-            $('.jobCard_mainContent', html).each(function() {
-                // const companyTitle = $(this).find('h2').text()
-                const spanCount = $(this).find('h2').find('span').length
-                let jobTitle = $(this).find('h2').find('span').text()
-                if (spanCount === 2) {
-                    jobTitle = jobTitle.slice(3) 
-        
-                    articles.push({
-                        jobTitle
-                        // companyTitle
-                    }) 
-                }
-                    else {
+app.get('/', function (req, res) {
+    res.json('This is my web scraper')
+})
+
+    app.get('/results', function(req, res) {
+        axios(url)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const articles = []
+                    
+                $('.jobCard_mainContent', html).each(function() {
+                    // const companyTitle = $(this).find('h2').text()
+                    const spanCount = $(this).find('h2').find('span').length
+                    let jobTitle = $(this).find('h2').find('span').text()
+                    if (spanCount === 2) {
+                        jobTitle = jobTitle.slice(3) 
+            
                         articles.push({
                             jobTitle
-                        })
+                            // companyTitle
+                        }) 
                     }
-                
-                })
-                    console.log(articles)
-                }).catch(err => console.log(err))
+                        else {
+                            articles.push({
+                                jobTitle
+                            })
+                        }
+                    
+                    })
+                        // console.log(articles)
+                        res.json(articles)
+        }).catch(err => console.log(err))
+})
+
+
         
-                app.listen(PORT, () => console.log(`server running on port ${PORT}`) )
+app.listen(PORT, () => console.log(`server running on port ${PORT}`) )
 
